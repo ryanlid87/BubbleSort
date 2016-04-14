9ffa65c1f3ea84642fb24616417faa7eed2b3adc
ryanlid87 <rlidster1@gmail.com>
2016-04-14 16:00:19 -0500

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -1,10 +1,17 @@
+
+from random import randint
+a = []
+for x in range(0,10):
+        a.append(randint(0,100))
+print a
+
 def bubsort(a):
         for i in range(len(a)):
-                for x in range(len(a)-1,0,-1):
-                        if a[x] < a[x-1]:
+                for x in range(0,len(a)-1):
+                        if a[x] > a[x+1]:
                                 temp = a[x]
-                                a[x] = a[x-1]
-                                a[x-1] = temp
+                                a[x] = a[x+1]
+                                a[x+1] = temp
         return a
 
 print bubsort(a)
