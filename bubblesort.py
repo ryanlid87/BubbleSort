01a0f0ec38c8b5462a176e2df3f8508c16628b44
ryanlid87 <rlidster1@gmail.com>
2016-04-14 16:12:07 -0500

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -1,9 +1,5 @@
-from random import randint
-a = []
-for x in range(0,10):
-        a.append(randint(0,100))
-print a
-
+#Bubble Sort
+# 123s < A < a
 def bubsort(a):
         for i in range(len(a)):
                 for x in range(0,len(a)-1):
@@ -11,4 +7,11 @@ def bubsort(a):
                                 a[x+1],a[x] = a[x],a[x+1]
         return a
 
-print bubsort(a)
+if __name__=="__main__":
+        from random import randint
+        a = []
+        for x in range(0,10):
+                a.append(randint(0,100))
+        print a
+        print bubsort(a)
+
