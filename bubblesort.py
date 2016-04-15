442ff5dddd59f77b81dde1a3ff15cf987905a142
ryanlid87 <rlidster1@gmail.com>
2016-04-15 15:39:41 -0500

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -7,11 +7,32 @@ def bubsort(a):
                                 a[x+1],a[x] = a[x],a[x+1]
         return a
 
-if __name__=="__main__":
-        from random import randint
-        a = []
-        for x in range(0,10):
-                a.append(randint(0,100))
-        print a
-        print bubsort(a)
+def mergesort(a):
+        if len(a)>1:
+                mid = len(a)//2
+                left=a[:mid]
+                right=a[mid:]
+                mergesort(left)
+                mergesort(right)
 
+                i=0
+                j=0
+                k=0
+                print left,right
+                while i<len(left) and j<len(right):
+                        if left[i] < right[j]:
+                                a[k]=left[i]
+                                i=i+1
+                        else:
+                                a[k]=right[j]
+                                j=j+1
+                        k=k+1
+                while i< len(left):
+                        a[k]=left[i]
+                        i=i+1
+                        k=k+1
+                while j < len(right):
+                        a[k]=right[j]
+                        j=j+1
+                        k=k+1
+                print a
